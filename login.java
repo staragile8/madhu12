5edffa74ad864dd405e0d46c62db0b965f8bb0b6
root <root@ip-172-31-30-197.ec2.internal>
2024-01-07 10:43:05 +0000

    adding files

---
+++ login.java
@@ -0,0 +1,14 @@
+public class login
+{
+	public main
+	{
+		public static void main(String[] args[])
+		{
+			init i;
+			for(i+0, i<=50; i++)
+			{
+				system.out.println("Number :" i+)
+			}
+		}
+	}
+}
